bc4b8ace4782632b17c51b497e562a999ef121c1
ajvalenz <67160144+ajvalenz@users.noreply.github.com>
2022-05-22 20:26:34 -0500

    Add files via upload

--- ejercicioDict.py
+++ ejercicioDict.py
@@ -22,21 +22,21 @@ def fileReader(file_name):
 
 def comparingHours(completeSchedule):
     completedEmployees = []  # list of employees that were reviewed
-    for employee, scheduledWork in completeSchedule.items():
-        for coworker, scheduleCoworker in completeSchedule.items():
-            pairs = 0
-            if employee != coworker:
-                if (coworker not in completedEmployees):
+    for employee, scheduledWork in completeSchedule.items(): #Takes one employee with his/her schedule
+        for coworker, scheduleCoworker in completeSchedule.items(): #takes a coworker with his /her schedule
+            pairs = 0 # number of times together
+            if employee != coworker: #checks its not the same employee
+                if (coworker not in completedEmployees): #
                     for hourWork in scheduledWork:
                         dayWork = hourWork[0:2]
                         timeWork = hourWork[2:]
-                        startEmployee = datetime.datetime.strptime(timeWork.split('-')[0].replace(" ", ''),'%H:%M').time()
-                        endEmployee = datetime.datetime.strptime(timeWork.split('-')[1].replace(" ", ''),'%H:%M').time()
+                        startEmployee = datetime.datetime.strptime(timeWork.split('-')[0].replace(" ", ''),'%H:%M').time() #creates a datetime object for the check in
+                        endEmployee = datetime.datetime.strptime(timeWork.split('-')[1].replace(" ", ''),'%H:%M').time() #creates a datetime object for the check out
                         for hourCoworker in scheduleCoworker:
                             dayCoworker = hourCoworker[0:2]
                             timeCoworker = hourCoworker[2:]
-                            startCoworker = datetime.datetime.strptime(timeCoworker.split('-')[0].replace(" ", ''),'%H:%M').time()
-                            endCoworker = datetime.datetime.strptime(timeCoworker.split('-')[1].replace(" ", ''),'%H:%M').time()
+                            startCoworker = datetime.datetime.strptime(timeCoworker.split('-')[0].replace(" ", ''),'%H:%M').time() #creates a datetime object for the check in (coworker)
+                            endCoworker = datetime.datetime.strptime(timeCoworker.split('-')[1].replace(" ", ''),'%H:%M').time() #creates a datetime object for the check out(coworker)
                             if dayWork == dayCoworker:
                                 if (startEmployee <= startCoworker and endEmployee >= startCoworker):
                                     pairs += 1
@@ -46,8 +46,8 @@ def comparingHours(completeSchedule):
                                     pairs += 1
                                 elif (startCoworker <= endEmployee and endCoworker >= endEmployee):
                                     pairs += 1
-                    completedEmployees.append(employee)
-                    print(employee, '-', coworker, ': ', pairs)
+                    completedEmployees.append(employee) #adds employee already checked
+                    print(employee, '-', coworker, ': ', pairs) #prints output
 
 
 fileReader('employee.txt')
